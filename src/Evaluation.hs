9d07db9e05db1679af1663e6efdbb3d2fa2853d3
raulbs7 <raulbs7>
2020-04-14 21:40:06 +0200

    Evaluation save Values

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -28,9 +28,18 @@ evalLine (Assign string assingExpr) varDictionary = return (case Map.lookup stri
 
 evalExpr :: Expr -> (Map.Map String (Maybe Integer)) -> Integer
 evalExpr (Int integer) varDictionary = integer
-evalExpr (Var string) varDictionary = return (case Map.lookup string varDictionary of
-    Just x -> x 
-    Nothing -> error ("Variable " ++ string ++ " not declared"))
+evalExpr (Var string) varDictionary = getValue string varDictionary
+evalExpr (Brack expr) varDictionary = evalExpr expr varDictionary
+evalExpr (Plus expr1 expr2) varDictionary = evalExpr expr1 varDictionary + evalExpr expr2 varDictionary
+evalExpr (Minus expr1 expr2) varDictionary = evalExpr expr1 varDictionary - evalExpr expr2 varDictionary
+evalExpr (Multiply expr1 expr2) varDictionary = evalExpr expr1 varDictionary * evalExpr expr2 varDictionary
+
+getValue :: String -> (Map.Map String (Maybe Integer)) -> Integer
+getValue var varDictionary = (case Map.lookup var varDictionary of
+        Just val -> case val of
+            Just val -> val 
+            Nothing -> error ("Variable " ++ var ++ " not initialized")
+        Nothing -> error ("Variable " ++ var ++ " not declared"))
 
 evalIdList :: [String] -> (Map.Map String (Maybe Integer)) -> (Map.Map String (Maybe Integer))
 evalIdList [] table = table
@@ -38,5 +47,5 @@ evalIdList (id:ids) table = evalIdList ids new_table
     where new_table = Map.insert id Nothing table
 
 main = do
-    new_dictionary <- eval (Program "Suma" ["Adios"] [Assign "Suma" (Int 5)])
+    new_dictionary <- eval (Program "Suma" ["Suma","Suma2"] [Assign "Suma" (Plus (Multiply (Var "Suma2")(Int 8))(Int 3))])
     print(new_dictionary)
