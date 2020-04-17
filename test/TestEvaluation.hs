7914abdf9b4a3f6dba5e37c40508fd20674cd4df
Enrique <ecepedavillamayor@gmail.com>
2020-04-18 00:17:51 +0200

    Tests added

--- test/TestEvaluation.hs
+++ test/TestEvaluation.hs
@@ -8,16 +8,24 @@ import           Test.Tasty
 import           Test.Tasty.HUnit as HU
 import           Tokens
 import qualified Data.Map as Map
+import qualified Control.Exception as Exc
 
 
-
-
-testExpression = HU.testCase "An arithmetic expression must the result according precendes of the operators" $ do
-    program <- readFile "test/resources/testEvaluation/testExpression.txt"
+evalFile file = do 
+    program <- readFile ("test/resources/testEvaluation/" ++ file)    
     let tokens = scanTokens program
     let grammar = parser tokens
     dictionary <- eval grammar
-    assertEqual "It should return the exact result that is 675" dictionary (Map.fromList [("result",Just 276)])
+    return dictionary
+
+testExpression = HU.testCase "An arithmetic expression evaluated according to precendes of operators" $ do
+    dictionary <- evalFile "testExpression.txt"
+    assertEqual "It should return 276" dictionary (Map.fromList [("result",Just 276)])
+ 
+--Doesnt work at the moment
+testAssignation = HU.testCase "An assignation of a non declared variable return an error" do
+    Exc.catch (dictionary <- evalFile "testExpression.txt") handler
+        where handler error = assertBool "Should throw an error" True
 
 evalLineTests = testGroup "EvalLine tests" [testExpression]
 evaluationTests = testGroup "Evaluation method tests" [evalLineTests]
